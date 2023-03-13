217c3fce8b70170e4667f0688dbc04405bee5b4d
Alex Norell <alex@norell.co>
2023-03-12 21:45:26 -0600

    Add the platform and arch

--- src/run.js
+++ src/run.js
@@ -5,28 +5,30 @@ import * as path from 'path';
 async function run() {
   try {
     let version = core.getInput('gh-cli-version');
+    let platform = core.getInput('platform')
+    let arch = core.getInput('arch')
     if (version) {
-      await getGhCli(version);
+      await getGhCli(version, platform, arch);
     }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-async function getGhCli(version) {
+async function getGhCli(version, platform, arch) {
   let toolPath = tc.find('gh-cli', version);
 
   if (!toolPath) {
-    toolPath = await downloadGhCli(version);
+    toolPath = await downloadGhCli(version, platform, arch);
   }
 
   toolPath = path.join(toolPath, 'bin');
   core.addPath(toolPath);
 }
 
-async function downloadGhCli(version) {
-  const toolDirectoryName = `gh_${version}_linux_amd64`;
-  const downloadUrl = `https://github.com/cli/cli/releases/download/v${version}/gh_${version}_linux_amd64.tar.gz`;
+async function downloadGhCli(version, platform, arch) {
+  const toolDirectoryName = `gh_${version}_${platform}_${arch}`;
+  const downloadUrl = `https://github.com/cli/cli/releases/download/v${version}/gh_${version}_${platform}_${arch}.tar.gz`;
   console.log(`downloading ${downloadUrl}`);
 
   try {
